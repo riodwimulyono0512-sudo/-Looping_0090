7a50c3bcb96ea65ba813ac92c83dbebe60f1d9c3
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-03-31 17:05:17 +0700

    part 3 file 3

--- logikaOR.cpp
+++ logikaOR.cpp
@@ -4,4 +4,9 @@ using namespace std;
 int main(){
     float nilB,nilM,rerata;
     string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai bahasa Inggris = ";
+    cin>>nilB;
 }
